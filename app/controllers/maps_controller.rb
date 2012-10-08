0f94c55f13c36c290daaeaf8dd44192aa93a5f50
u1 <u1@www6042ue.sakura.ne.jp>
2012-10-08 20:13:34 +0900

    Implement showing multiple points at map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,8 +1,20 @@
 class MapsController < ApplicationController
 
   def show
+
+    # 地図の取得
     key = params[:key]
     @map = Map.find_by_key(key)
+    
+    if request.mobile? and request.mobile.position
+      # ユーザ追加
+      @user = User.new
+      @user.lat = request.mobile.position.lat
+      @user.lon = request.mobile.position.lon
+      @user.map_id = @map.id
+      @user.save
+    end
+
     @users = User.find_all_by_map_id(@map.id)
   end
 
